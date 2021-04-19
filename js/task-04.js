baf86c0904dadffe0e91e865a9bf8022eb9ffc70
Yuliia Kunitska <yuliiakun13@gmail.com>
2021-04-19 22:04:52 +0300

    hw_js_07 task-04

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+const counterValue = {
+    value: 1,
+    increment() {
+        this.value += 1;
+    },
+    decrement() {
+        this.value -= 1;
+    },
+};
+
+const refs = {
+    decrementBtn: document.querySelector('[data-action="decrement"]'),
+    incrementBtn: document.querySelector('[data-action="increment"]'),
+    valueEl: document.querySelector('#value'),
+};
+
+refs.decrementBtn.addEventListener('click', () => {
+    counterValue.decrement();
+
+    refs.valueEl.textContent = counterValue.value;
+});
+refs.incrementBtn.addEventListener('click', () => {
+    counterValue.increment();
+
+    refs.valueEl.textContent = counterValue.value;
+});
